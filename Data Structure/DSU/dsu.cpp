15d9a7130991a0f425d5efd50d8be1099ca6ad09
erichung1113 <erichung1113@gmail.com>
2023-02-17 15:56:40 +0800

    add by erichung

--- Data Structure/DSU/dsu.cpp
+++ Data Structure/DSU/dsu.cpp
@@ -1,18 +1,18 @@
-#define MAXN 100005
-
 typedef struct DisjoinSetUnion {
-    int dsu[MAXN], dsusz[MAXN];
-    // set<int> elements[MAXN];
+    // vector<set<int>> elements;
+    vector<int> dsu, dsusz;
+    int group;
+
     DisjoinSetUnion(int n) {
+        // elements.resize(n+1);
+        dsu.resize(n + 1);
+        dsusz.resize(n + 1);
+        group = n;
         for (int i = 1; i <= n; i++) {
             dsu[i] = i;
             dsusz[i] = 1;
             // elements[i].insert(i);
         }
-    }
-    int fa(int x) {
-        if (dsu[x] == x) return x;
-        return dsu[x] = fa(dsu[x]);
     }
     int sz(int x) {
         return dsusz[fa(x)];
@@ -23,17 +23,24 @@ typedef struct DisjoinSetUnion {
     bool isRoot(int x) {
         return dsu[x] == x;
     }
-    void uni(int a, int b) {
+    int fa(int x) {
+        if (isRoot(x)) return x;
+        return dsu[x] = fa(dsu[x]);
+    }
+    bool uni(int a, int b) {
         a = fa(a), b = fa(b);
+        if (a == b) return false;
         dsu[b] = a;
         dsusz[a] += dsusz[b];
         dsusz[b] = 0;
+        group--;
         // if (sz(a) < sz(b)) swap(a, b);
         // for (auto i : elements[b]) {
         //     elements[a].insert(i);
         //     dsu[i] = a;
         // }
         // elements[b].clear();
+        return true;
     }
 } DSU;
 
@@ -44,12 +51,10 @@ Usage:
     dsu.same(a, b);
     dsu.sz(x);
     dsu.isRoot(x);
+    dsu.group
 
 Notice:
     a[]: 1-base
-    if occur segmentation fault, please put dsu and dsusz array in Global.
+    if occur segmentation fault, please put dsu and dsusz in Global.
     if you need record elements, please undefine the code.
-
-Things Need To Do:
-    define MAXN
 */
